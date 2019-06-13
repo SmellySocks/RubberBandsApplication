51f9f58d89900ae9d99ce7edfd6020e603362723
SmellySocks <michal96hyla@gmail.com>
2019-06-13 19:04:45 +0200

    first test

--- rubberband/src/test/java/pl/edu/pjatk/tau/AppTest.java
+++ rubberband/src/test/java/pl/edu/pjatk/tau/AppTest.java
@@ -7,32 +7,24 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+@RunWith(JUnit4.class)
+public class AddTest {
+    Add a;
+
+    @Before
+    public void Init(){
+        a = new Add();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void SumCheck(){
+
+        Assert.assertEquals(4,a.sum());
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void LoopSumCheck(){
+        Assert.assertEquals(1.0, a.loopSum(), 0.0001
+        );
     }
 }
